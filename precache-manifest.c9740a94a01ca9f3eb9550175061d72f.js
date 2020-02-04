18c8e74d591a59b1d7e319ddadb0a856b134f642
KuaqSon <qson.ng@gmail.com>
2020-02-04 15:03:08 +0700

    Updates

---
+++ precache-manifest.c9740a94a01ca9f3eb9550175061d72f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec4bfd8bdef7f4fffc60ae7ef81bf2f",
+    "url": "/tinhnongdocon/index.html"
+  },
+  {
+    "revision": "6f6ae38a042f03f65edb",
+    "url": "/tinhnongdocon/static/css/main.61455665.chunk.css"
+  },
+  {
+    "revision": "bf8be2bee99782ce7ef0",
+    "url": "/tinhnongdocon/static/js/2.e7d745a5.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/tinhnongdocon/static/js/2.e7d745a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f6ae38a042f03f65edb",
+    "url": "/tinhnongdocon/static/js/main.049f1e39.chunk.js"
+  },
+  {
+    "revision": "c05149f51e6359b401c5",
+    "url": "/tinhnongdocon/static/js/runtime-main.aef5d504.js"
+  }
+]);
